f52f82a3d9f2b448889bdaf40a1b3cbe0c858314
rex98@qq.com <327653624@qq.com>
2024-04-06 20:09:13 +0800

    Init Commit

---
+++ CW-FreeShop/FreeShop.cs
@@ -0,0 +1,54 @@
+﻿using BepInEx;
+using HarmonyLib;
+using Photon.Pun;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CW_FreeShop
+{
+	[BepInPlugin("FreeShop", "Free Shop", "1.0")]
+	public class FreeShop : BaseUnityPlugin
+	{
+		void Awake()
+		{
+			var harmony = new Harmony("FreeShop");
+			harmony.PatchAll(typeof(FreeShopPatch));
+		}
+	}
+
+	class FreeShopPatch
+	{
+		[HarmonyPatch(typeof(ShopHandler), "OnAddToCartItemClicked")]
+		[HarmonyPrefix]
+		static bool OnAddToCartItemClickedPrefix(byte itemID, Dictionary<byte, ShopItem> ___m_ItemsForSaleDictionary, PhotonView ___m_PhotonView, SFX_Instance ___addSFX, ShopHandler __instance)
+		{
+			if (!___m_ItemsForSaleDictionary.ContainsKey(itemID))
+				return true;
+
+			___m_PhotonView.RPC("RPCA_AddItemToCart", RpcTarget.All, new object[]
+			{
+					itemID
+			});
+			___addSFX.Play(__instance.transform.position, false, 1f, null);
+			return false;
+		}
+
+		[HarmonyPatch(typeof(ShopHandler), "OnOrderCartClicked")]
+		[HarmonyPrefix]
+		static bool OnOrderCartClickedPrefix(ShoppingCart ___m_ShoppingCart, PhotonView ___m_PhotonView, SFX_Instance ___purchaseSFX, ShopHandler __instance)
+		{
+			if (___m_ShoppingCart.IsEmpty)
+				return true;
+
+			___purchaseSFX.Play(__instance.transform.position, false, 1f, null);
+			byte[] itemIDs = (from item in ___m_ShoppingCart.Cart
+							  select item.ItemID).ToArray<byte>();
+			___m_PhotonView.RPC("RPCA_SpawnDrone", RpcTarget.All, new object[]
+			{
+				itemIDs
+			});
+			__instance.OnClearCartClicked();
+			return false;
+		}
+	}
+}
