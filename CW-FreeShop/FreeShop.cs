10299afd2d8d70e657df44cd911ac31d8e8292be
Mae <mae@weatherelectric.xyz>
2024-04-07 21:56:46 -0700

    Make items free lol

--- CW-FreeShop/FreeShop.cs
+++ CW-FreeShop/FreeShop.cs
@@ -3,49 +3,64 @@ using Photon.Pun;
 using System.Collections.Generic;
 using System.Linq;
 using MelonLoader;
+using Zorro.Core;
 
 namespace CW_FreeShop
 {
-	public class FreeShop : MelonMod
-	{
-		internal const string Title = "FreeShop";
-		internal const string Version = "1.0.0";
-		internal const string Author = "ShingekiNoRex";
-	}
+    public class FreeShop : MelonMod
+    {
+        internal const string Title = "FreeShop";
+        internal const string Version = "1.0.0";
+        internal const string Author = "ShingekiNoRex";
 
-	public static class FreeShopPatch
-	{
-		[HarmonyPatch(typeof(ShopHandler), "OnAddToCartItemClicked")]
-		[HarmonyPrefix]
-		static bool OnAddToCartItemClickedPrefix(byte itemID, Dictionary<byte, ShopItem> ___m_ItemsForSaleDictionary, PhotonView ___m_PhotonView, SFX_Instance ___addSFX, ShopHandler __instance)
-		{
-			if (!___m_ItemsForSaleDictionary.ContainsKey(itemID))
-				return true;
+        public override void OnInitializeMelon()
+        {
+            ItemDatabase itemDatabase = SingletonAsset<ItemDatabase>.Instance;
+            foreach (Item item in itemDatabase.Objects)
+            {
+                if (item.purchasable)
+                {
+                    item.price = 0;
+                }
+            }
+        }
+    }
+}
 
-			___m_PhotonView.RPC("RPCA_AddItemToCart", RpcTarget.All, new object[]
-			{
-					itemID
-			});
-			___addSFX.Play(__instance.transform.position, false, 1f, null);
-			return false;
-		}
+public static class FreeShopPatch
+{
+    [HarmonyPatch(typeof(ShopHandler), "OnAddToCartItemClicked")]
+    [HarmonyPrefix]
+    private static bool OnAddToCartItemClickedPrefix(byte itemID, Dictionary<byte, ShopItem> ___m_ItemsForSaleDictionary,
+        PhotonView ___m_PhotonView, SFX_Instance ___addSFX, ShopHandler __instance)
+    {
+        if (!___m_ItemsForSaleDictionary.ContainsKey(itemID))
+            return true;
 
-		[HarmonyPatch(typeof(ShopHandler), "OnOrderCartClicked")]
-		[HarmonyPrefix]
-		static bool OnOrderCartClickedPrefix(ShoppingCart ___m_ShoppingCart, PhotonView ___m_PhotonView, SFX_Instance ___purchaseSFX, ShopHandler __instance)
-		{
-			if (___m_ShoppingCart.IsEmpty)
-				return true;
+        ___m_PhotonView.RPC("RPCA_AddItemToCart", RpcTarget.All, new object[]
+        {
+            itemID
+        });
+        ___addSFX.Play(__instance.transform.position, false, 1f, null);
+        return false;
+    }
 
-			___purchaseSFX.Play(__instance.transform.position, false, 1f, null);
-			byte[] itemIDs = (from item in ___m_ShoppingCart.Cart
-							  select item.ItemID).ToArray<byte>();
-			___m_PhotonView.RPC("RPCA_SpawnDrone", RpcTarget.All, new object[]
-			{
-				itemIDs
-			});
-			__instance.OnClearCartClicked();
-			return false;
-		}
-	}
+    [HarmonyPatch(typeof(ShopHandler), "OnOrderCartClicked")]
+    [HarmonyPrefix]
+    static bool OnOrderCartClickedPrefix(ShoppingCart ___m_ShoppingCart, PhotonView ___m_PhotonView,
+        SFX_Instance ___purchaseSFX, ShopHandler __instance)
+    {
+        if (___m_ShoppingCart.IsEmpty)
+            return true;
+
+        ___purchaseSFX.Play(__instance.transform.position, false, 1f, null);
+        byte[] itemIDs = (from item in ___m_ShoppingCart.Cart
+            select item.ItemID).ToArray<byte>();
+        ___m_PhotonView.RPC("RPCA_SpawnDrone", RpcTarget.All, new object[]
+        {
+            itemIDs
+        });
+        __instance.OnClearCartClicked();
+        return false;
+    }
 }
